5480fcdd393c1f426ee825ecca7459ecaf751a7d
Helvets <pedro.am.gomes@hotmail.com>
2018-06-28 16:01:56 -0300

    Update knn.cpp

--- knn.cpp
+++ knn.cpp
@@ -5,17 +5,20 @@
 #include <math.h>
 #include <set>
 #include <map>
+#include<cstdlib>
 //#include <string>
-#include <string.h>
+//#include <string.h>
 //#include <strings.h>
 
 using namespace std;
 
+//criando a classe for
 class Flor
 {
+	//variáveis
 private:
 	string classe;
-	double arg1, arg2, arg3, arg4;
+	double arg1, arg2, arg3, arg4; //parametros das flores + tipo
 
 public:
 	//Construtor
@@ -55,153 +58,148 @@ public:
 	}
 };
 
-// função que retorna a distância euclidiana entre 2 indivíduos
+// função encarregada de retornar a distância euclidiana entre 2 pontos (indivíduos)
 double distancia(Flor ind1, Flor ind2)
 {
-	/*
-		A distância euclidiana é a raiz quadrada da soma das
-		diferenças dos valores dos atributos elevado ao quadrado
-	*/
+	//distância euclidiana
 	
 	double soma = pow((ind1.getArg1() - ind2.getArg1()), 2) + pow((ind1.getArg2() - ind2.getArg2()), 2) + pow((ind1.getArg3() - ind2.getArg3()), 2) + pow((ind1.getArg4() - ind2.getArg4()), 2);
 	
 	return sqrt(soma);
 }
 
-// essa função classifica uma nova amostra
+// classifica nova amostra
 string classifica(vector<Flor>& flores, Flor novo_exemplo, int K)
 {
-	// se o K for par decrementa
-	//if(K % 2 == 0)
-	//{
-	//	K--;
-	//	if(K <= 0)
-	//		K = 1;
-	//}
-	
-	if(K % 2 == 0 || K < 0){
-		return "K deve ser um valor ímpar positivo!";
-	}
-		
-
-	// obtém o tamanho do vetor
+	// obtém o tamanho do vetor de Flor passano como parâmetro na função
 	int vetSize = flores.size();
 
+	//contador de votos
+	int contK = 0;
+	
 	/*
-		set de pairs da distância de cada indivíduo
-		do conjunto de treinamento para o novo exemplo
-		cada pair é composto pela distância e o índice
-		do indivíduo no vetor
+	
+		cria um par composto de:
+		pela distância e o índice do indivíduo no vetor
+	
+		a distância é a distância de cada indivíduo do conjunto de treinamento para o novo exemplo.
 	*/
 	set<pair<double, int> > dist_flores;
 
-	/*
-		calcula-se a distância euclidiana do novo exemplo
-		para cada amostra do conjunto de treinamento
-	*/
+	//for que calcula a distancia euclidiana do novo exemplo pra cada amostra do conjunto de treinamento
 	for(int i = 0; i < vetSize; i++)
 	{
 		double dist = distancia(flores[i], novo_exemplo);
+		
+		//insere novo par
 		dist_flores.insert(make_pair(dist, i));
 	}
+	
 	/*
-	para decidir a qual classe pertence o novo exemplo,
-	basta verificar arg1 classe mais frequente dos K
-	vizinhos mais próximos
+	o novo exemplo será classificado dependendo de quem possuir maioria nos K primeiros votos
+	que estão ordenados do mais próximo para o mais distante
 	*/
 	set<pair<double, int> >::iterator j;
 	
 	/*
-		o contador de Iris-setosa estará no índice 0,
-		o contador de Iris-versicolor estará no índice 1
-		e o contador de Iris-virginica estará no índice 2
+		o contador de Iris-setosa = índice 0,
+		o contador de Iris-versicolor = índice 1
+		o contador de Iris-virginica = índice 2
 	*/
 	vector<int> cont_classes(3);
 	
-	int contK = 0;
-
+	//for do inicio de dist_flores até o final
 	for(j = dist_flores.begin(); j != dist_flores.end(); j++)
-	{
-		if(contK == K) break;
+	{	
+		//quando atingirmos o total de votos, pare
+		if(contK == K)
+		{
+			break;
+		}
 		
+		//pega a classe do vizinho mais próximo
 		string classe = flores[j->second].getClasse();
 
+		//guarda os votos
 		if(classe == "Iris-setosa")
 			cont_classes[0]++;
 		else if(classe == "Iris-versicolor")
 			cont_classes[1]++;
 		else
 			cont_classes[2]++;
-
+		
+		//passa pro próximo voto
 		contK++;
 	}
 	
-	string classe_classificacao;
+	//preparamos o return da classe obtida
+	string classificacao_obtida;
 	
+	//verificações para ver qual classe foi obtida
 	if(cont_classes[0] >= cont_classes[1] && cont_classes[0] >= cont_classes[2])
-		classe_classificacao = "Iris-setosa";
+		classificacao_obtida = "Iris-setosa";
 	else if(cont_classes[1] >= cont_classes[0] && cont_classes[1] >= cont_classes[2])
-		classe_classificacao = "Iris-versicolor";
+		classificacao_obtida = "Iris-versicolor";
 	else
-		classe_classificacao = "Iris-virginica";
+		classificacao_obtida = "Iris-virginica";
 
-	return classe_classificacao;
+	return classificacao_obtida;
 }
-
 
 int main(int argc, char *argv[])
 {
+    vector<Flor> flores;
 
-	vector<Flor> flores;
+    //K vizinhos que serão observados para classificar uma nova amostra
+    int K = atoi(argv[1]);
 
-	/*
-		o K é a quantidade de vizinhos que serão
-		levados em conta para classificação de um
-		novo dado, é recomendável que seja ímpar
-		para que não possa haver empate
-	*/
-	int K = 3;
-	
-	// tamanho do conjunto de dados de treinamento
-	int tam_treinamento = 105;
+    //tamanho do conjunto de dados de treinamento
+    int tam_treinamento = 105;
 
-	/*
-		o processo de treinamento consiste em apenas
-		armazenar o conjunto de dados de treinamento
-	*/
-	for(int i = 0; i < tam_treinamento; i++)
-	{
-		string classe;
-		double arg1, arg2, arg3, arg4;
+    //cout << "Digite um valor para K!";
+    //cout << K;
 
-		cin >> arg1 >> arg2 >> arg3 >> arg4 >> classe;
+     if(K % 2 == 1 || K < 0){    	
 
-		flores.push_back(Flor(arg1, arg2, arg3, arg4, classe));
-	}
+    //o conjunto de treinamento fica guardado para ser consultado
+    for(int i = 0; i < tam_treinamento; i++)
+    {
+        string classe;
+        double arg1, arg2, arg3, arg4;
 
-	int acertos = 0;
-	int tam_testes = 150 - tam_treinamento;
-	
-	// processo de classificação
-	for(int i = 0; i < tam_testes; i++)
-	{
-		string classe;
-		double arg1, arg2, arg3, arg4;
+        //lê os parametros no arquivo e armazena eles nas variáveis
 
-		cin >> arg1 >> arg2 >> arg3 >> arg4 >> classe;
-		
-		Flor ind(arg1, arg2, arg3, arg4, classe);
-		
-		string classe_obtida = classifica(flores, ind, K);
-		
-		cout << "Classe esperada: " << classe << "\n";
-		cout << "Classe obtida: " << classe_obtida << "\n\n";
-		
-		if(classe == classe_obtida)
-			acertos++;
-	}
-	
-	cout << acertos << " acertos de um total de " << tam_testes << " testes.\n";
+        cin >> arg1 >> arg2 >> arg3 >> arg4 >> classe;
 
-	return 0;
+        flores.push_back(Flor(arg1, arg2, arg3, arg4, classe));
+    }
+
+    int acertos = 0;
+    int tam_testes = 150 - tam_treinamento;
+
+    // processo de classificação
+    for(int i = 0; i < tam_testes; i++)
+    {
+        string classe;
+        double arg1, arg2, arg3, arg4;
+
+        cin >> arg1 >> arg2 >> arg3 >> arg4 >> classe;
+
+        Flor ind(arg1, arg2, arg3, arg4, classe);
+
+        string classe_obtida = classifica(flores, ind, K);
+
+        cout << "Classe esperada: " << classe << "\n";
+        cout << "Classe obtida: " << classe_obtida << "\n\n";
+
+        if(classe == classe_obtida)
+            acertos++;
+    }
+
+    cout << acertos << " acertos de um total de " << tam_testes << " testes.\n";
+}else{
+cout << "K deve ser ímpar positivo!";
+}
+
+    return 0;
 }
